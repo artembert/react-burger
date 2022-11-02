1a8cf0eff3c110516e98f2dfe19d868350ca7549
Artem Beresnev <artembert@gmail.com>
2022-11-02 15:24:28 +0300

    S1-S1 Add <IngredientsTypeSection />

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { BurgerIngredientsNavigation } from "./burger-ingredients-navigation/burger-ingredients-navigation";
 import { BurgerParts } from "../../types/burger-parts";
-// import styles from './burger-ingredients.module.css';
+import { ingredientType } from "../../types/constants/ingredient";
+import PropTypes from "prop-types";
+import { IngredientsTypeSection } from "./ingredients-type-section/ingredients-type-section";
 
 export const BurgerIngredients = (props) => {
+  const { ingredients } = props;
   const [current, setCurrent] = React.useState(BurgerParts.BUN);
+  const banIngredients = useMemo(() => ingredients.filter((item) => item.type === BurgerParts.BUN), [ingredients]);
+  const sauceIngredients = useMemo(() => ingredients.filter((item) => item.type === BurgerParts.SAUCE), [ingredients]);
+  const mainIngredients = useMemo(() => ingredients.filter((item) => item.type === BurgerParts.MAIN), [ingredients]);
+
   return (
     <section>
       <BurgerIngredientsNavigation current={current} onChange={setCurrent} />
+      <IngredientsTypeSection title="Булки" ingredients={banIngredients} />
+      <IngredientsTypeSection title="Соусы" ingredients={sauceIngredients} />
+      <IngredientsTypeSection title="Начинки" ingredients={mainIngredients} />
     </section>
   );
+};
+
+BurgerIngredients.propTypes = {
+  ingredients: PropTypes.arrayOf(ingredientType).isRequired,
 };
--- src/pages/constructor-page/index.jsx
+++ src/pages/constructor-page/index.jsx
@@ -3,10 +3,14 @@ import { AppHeader } from "../../components/app-header/app-header";
 import { BurgerConstructor } from "../../components/burger-constructor/burger-constructor";
 import { BurgerIngredients } from "../../components/burger-ingredients/burger-ingredients";
 import { ConstructorPageLayout } from "../../components/constructor-page-layout/constructor-page-layout";
+import ingredients from "../../mocks/ingredients.json";
 
 export const ConstructorPage = () => (
   <>
     <AppHeader />
-    <ConstructorPageLayout leftColumn={<BurgerIngredients />} rightColumn={<BurgerConstructor />} />
+    <ConstructorPageLayout
+      leftColumn={<BurgerIngredients ingredients={ingredients} />}
+      rightColumn={<BurgerConstructor />}
+    />
   </>
 );
