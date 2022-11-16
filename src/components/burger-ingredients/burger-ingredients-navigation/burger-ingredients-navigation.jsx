c1f5194973c6a79b829b010a1916a5326d751e19
Artem Beresnev <artembert@gmail.com>
2022-11-16 22:47:39 +0300

    S2-S2 shrink burger-ingredients

--- src/components/burger-ingredients/burger-ingredients-navigation/burger-ingredients-navigation.jsx
+++ src/components/burger-ingredients/burger-ingredients-navigation/burger-ingredients-navigation.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import { BurgerParts } from "../../../types/burger-parts";
+import styles from "./burger-ingredients-navigation.module.css";
 
 export const BurgerIngredientsNavigation = (props) => {
   const { current, onChange } = props;
   return (
-    <div style={{ display: "flex" }}>
-      <Tab value={BurgerParts.BUN} active={current === BurgerParts.BUN} onClick={onChange}>
-        Булки
-      </Tab>
-      <Tab value={BurgerParts.SAUCE} active={current === BurgerParts.SAUCE} onClick={onChange}>
-        Соусы
-      </Tab>
-      <Tab value={BurgerParts.MAIN} active={current === BurgerParts.MAIN} onClick={onChange}>
-        Начинки
-      </Tab>
+    <div className={styles.root}>
+      <div className={styles.tagContainer}>
+        <Tab value={BurgerParts.BUN} active={current === BurgerParts.BUN} onClick={onChange}>
+          Булки
+        </Tab>
+      </div>
+      <div className={styles.tagContainer}>
+        <Tab value={BurgerParts.SAUCE} active={current === BurgerParts.SAUCE} onClick={onChange}>
+          Соусы
+        </Tab>
+      </div>
+      <div className={styles.tagContainer}>
+        <Tab value={BurgerParts.MAIN} active={current === BurgerParts.MAIN} onClick={onChange}>
+          Начинки
+        </Tab>
+      </div>
     </div>
   );
 };
