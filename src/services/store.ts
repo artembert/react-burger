5f0da3fe93247540ef551b4cbe7d08f8d3d15578
Artem Beresnev <artembert@gmail.com>
2022-11-17 21:37:51 +0300

    S2-S2 turn off serializableCheck

--- src/services/store.ts
+++ src/services/store.ts
@@ -8,6 +8,7 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
