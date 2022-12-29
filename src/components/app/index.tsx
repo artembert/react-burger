40222409398dcd95d1b874a0eb0470e2ac4e460c
Artem Beresnev <artembert@gmail.com>
2022-12-30 00:05:41 +0300

    S3-S1 move Provider & Router from <App> to index.tsx

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,8 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { ConstructorPage } from "../../pages/constructor-page/constructor-page";
-import { Provider } from "react-redux";
-import { store } from "../../services/store";
 import { AppHeader } from "../app-header/app-header";
 import { Layout } from "../layout/layout";
 import { RegisterPage } from "../../pages/register-page/register-page";
@@ -15,32 +12,28 @@ import { ProtectedRoute } from "../protected-route/protected-route";
 
 export const App = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <Layout>
-          <AppHeader />
-          <Switch>
-            <ProtectedRoute path={Routes.Profile} exact>
-              <ProfilePage />
-            </ProtectedRoute>
-            <Route path={Routes.Register} exact>
-              <RegisterPage />
-            </Route>
-            <Route path={Routes.Login} exact>
-              <LoginPage />
-            </Route>
-            <Route path={Routes.ForgotPassword} exact>
-              <ForgotPasswordPage />
-            </Route>
-            <Route path={Routes.ResetPassword} exact>
-              <ResetPasswordPage />
-            </Route>
-            <Route path="/">
-              <ConstructorPage />
-            </Route>
-          </Switch>
-        </Layout>
-      </Router>
-    </Provider>
+    <Layout>
+      <AppHeader />
+      <Switch>
+        <ProtectedRoute path={Routes.Profile} exact>
+          <ProfilePage />
+        </ProtectedRoute>
+        <Route path={Routes.Register} exact>
+          <RegisterPage />
+        </Route>
+        <Route path={Routes.Login} exact>
+          <LoginPage />
+        </Route>
+        <Route path={Routes.ForgotPassword} exact>
+          <ForgotPasswordPage />
+        </Route>
+        <Route path={Routes.ResetPassword} exact>
+          <ResetPasswordPage />
+        </Route>
+        <Route path="/">
+          <ConstructorPage />
+        </Route>
+      </Switch>
+    </Layout>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { App } from "./components/app";
+import { store } from "./services/store";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
