3646d39488acc27e068b329dc4d1758bd0014921
Artem Beresnev <artembert@gmail.com>
2023-01-02 20:49:54 +0300

    S3-S1 request: add Patch method support

--- src/app/helpers/request.ts
+++ src/app/helpers/request.ts
@@ -8,23 +8,25 @@ type Options =
     }
   | {
       body: any;
-      method: typeof HttpMethod.POST;
+      method: typeof HttpMethod.POST | typeof HttpMethod.PATCH;
       authorization?: boolean;
     };
 
 export const HttpMethod = {
   GET: "GET",
   POST: "POST",
+  PATCH: "PATCH",
 } as const;
 
 export const request = async <T>(url: string, options: Options = { method: HttpMethod.GET }) => {
+  const shouldHaveBody = options.method === HttpMethod.POST || options.method === HttpMethod.PATCH;
   const headers = new Headers({ "content-type": "application/json" });
   if (options.authorization) {
     headers.append("authorization", `Bearer ${getAccessToken()}`);
   }
   const res = await fetch(url, {
     method: options.method,
-    body: options.method === HttpMethod.POST ? JSON.stringify(options.body) : undefined,
+    body: shouldHaveBody ? JSON.stringify(options.body) : undefined,
     headers,
   });
   // TODO: check if token expired
