f61e9054ba20f7022534529fb784be57078a72dc
Artem Beresnev <artembert@gmail.com>
2023-02-05 23:19:46 +0400

    S5-S1 support method to fetch single order by id

---
+++ src/services/order/requests/fetch-order.ts
@@ -0,0 +1,21 @@
+import { request } from "../../../app/helpers/request";
+import { API_ENDPOINT } from "../../../app/constants";
+import { ORDERS } from "./constants";
+import { OrderHistoryDetails } from "../../../types/order-history-details";
+import { mapOrderFromHistoryRes } from "../../feed/helpers/covert-order-history-res.helper";
+import { OrderHistoryRes } from "../../../types/order-history-res";
+
+type OrderRes = {
+  orders: OrderHistoryRes["orders"];
+  success: boolean;
+};
+
+const endpoint = (id: string) => `${API_ENDPOINT}${ORDERS}/${id}`;
+
+export const fetchOrder = async (id: string): Promise<OrderHistoryDetails> => {
+  const response = await request<OrderRes>(endpoint(id));
+  if (response.success) {
+    return mapOrderFromHistoryRes(response.orders)[0];
+  }
+  return Promise.reject("Failed to fetch order");
+};
